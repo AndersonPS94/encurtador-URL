96b3ccacc3492a2c16cd68d6d080e2937ce897fb
AndersonPS94 <dson_adn@hotmail.com>
2022-03-24 23:03:28 -0300

    first commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,6 @@
+export const config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION:
+    'mongodb+srv://AndersonPS94:<password>@cluster0.ede48.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+}
